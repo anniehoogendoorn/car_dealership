bbd8623cc9e159263c253a8b9f8f3b991d3443ac
Annie Hoogendoorn & Marcos Moncivais <student@epicodus.com>
2015-08-03 14:54:57 -0700

    add function constructor and instantiate car objects

--- car.php
+++ car.php
@@ -4,28 +4,22 @@
     public $make_model;
     public $price;
     public $miles;
+
+    function __construct ($brand, $car_price = 26000, $odometer = 10000)
+    {
+    $this->make_model = $brand;
+    $this->price = $car_price;
+    $this->miles = $odometer;
+    }
   }
 
 
-$porsche = new Car();
-$porsche->make_model ="2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles 7864;
+$porsche = new Car("2014 Porsche 911",20000);
+$ford = new Car("2011 Ford F450", 55995, 14241);
+$lexus = new Car("2013 Lexus RX 350", 44700, 20000);
+$mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
 
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
 
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
-
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
 
 
 $cars = array($porsche, $ford, $lexus, $mercedes);

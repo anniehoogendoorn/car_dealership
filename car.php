2a48593630b20865f24f34846173b8a396def6a4
Annie Hoogendoorn & Marcos Moncivais <student@epicodus.com>
2015-08-03 16:51:45 -0700

    add setPrice

--- car.php
+++ car.php
@@ -2,7 +2,7 @@
   class Car
   {
     public $make_model;
-    public $price;
+    private $price;
     public $miles;
 
     function __construct ($brand, $car_price = 26000, $odometer = 10000)
@@ -11,15 +11,24 @@
     $this->price = $car_price;
     $this->miles = $odometer;
     }
-  }
+
+    function setPrice($new_price)
+    {
+      $float_price = (float) $new_price;
+      if ($float_price != 0) {
+        $formatted_price = number_format($float_price, 2);
+        $this->price = $formatted_price;
+      }
+    }
+}
 
 
-$porsche = new Car("2014 Porsche 911",20000);
+
+$porsche = new Car("2014 Porsche 911", 40000,20000);
 $ford = new Car("2011 Ford F450", 55995, 14241);
 $lexus = new Car("2013 Lexus RX 350", 44700, 20000);
 $mercedes = new Car("Mercedes Benz CLS550", 39900, 37979);
-
-
+$mercedes->price("1.3925");
 
 
 $cars = array($porsche, $ford, $lexus, $mercedes);
